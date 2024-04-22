304685a37e05254a3b9bbca35f4e9ef4f207e099
ren66.rl@gmail.com <ren66.rl@gmail.com>
2024-04-22 07:17:16 -0700

    got it working!

--- deck-js0.py
+++ deck-js0.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 
 import struct
+from queue import Queue
+from threading import Thread
 
 infile_path = "/dev/input/js0"
-EVENT_SIZE = struct.calcsize("llHHI")
+EVENT_SIZE = struct.calcsize("LhBB")
 file = open(infile_path, "rb")
 event = file.read(EVENT_SIZE)
-while event:
-    print(struct.unpack("llHHI", event))
-    (tv_sec, tv_usec, type, code, value) = struct.unpack("llHHI", event)
-    event = file.read(EVENT_SIZE)
+
+def event_emitter(out_q):
+    global file
+    global event
+    while event:
+        # print(struct.unpack("LhBB", event))
+        # data = struct.unpack("LhBB", event)
+        out_q.put(event)
+        event = file.read(EVENT_SIZE)
+
+def event_consumer(in_q):
+    while True:
+        event = in_q.get()
+        (tv_msec,  value, type, number) = struct.unpack("LhBB", event)
+        if type != 2 and type < 119:
+            print(f"({value}, {type}, {number})")
+
+q = Queue()
+t1 = Thread(target=event_consumer, args=(q, ))
+t2 = Thread(target=event_emitter, args=(q, ))
+t1.start()
+t2.start()
