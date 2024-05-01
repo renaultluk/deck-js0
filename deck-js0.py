40c1505aeedeba7d6ec66037f20c27bed6c4427b
ren66.rl@gmail.com <ren66.rl@gmail.com>
2024-05-01 02:12:30 -0700

    pyjoystick

--- deck-js0.py
+++ deck-js0.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
+from pyjoystick.sdl2 import Key, Joystick, run_event_loop
 
-import struct
+def print_add(joy):
+    print('Added', joy)
 
-infile_path = "/dev/input/js0"
-EVENT_SIZE = struct.calcsize("llHHI")
-file = open(infile_path, "rb")
-event = file.read(EVENT_SIZE)
-while event:
-    print(struct.unpack("llHHI", event))
-    (tv_sec, tv_usec, type, code, value) = struct.unpack("llHHI", event)
-    event = file.read(EVENT_SIZE)
+def print_remove(joy):
+    print('Removed', joy)
+
+def key_received(key):
+    if key.keytype == Key.BUTTON:
+        print('received', key, " ", key.value)
+    # if key.value == Key.HAT_UP:
+    #     print("hat up")
+    # elif key.value == Key.HAT_DOWN:
+    #     print("hat down")
+    # if key.value == Key.HAT_LEFT:
+    #     #do something
+    # elif key.value == Key.HAT_UPLEFT:
+    #     #do something
+    # elif key.value == Key.HAT_DOWNLEFT:
+    #     #do something
+    # elif key.value == Key.HAT_RIGHT:
+    #     #do something
+    # elif key.value == Key.HAT_UPRIGHT:
+    #     #do something
+    # elif key.value == Key.HAT_DOWNRIGHT:
+    #     #do something
+
+run_event_loop(print_add, print_remove, key_received)
